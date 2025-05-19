f716301de6df42dd7ccdd615104b5c2bfd020629
Septyana Agustina <septyanagustina28@gmail.com>
2025-05-19 08:08:01 +0700

    Delete vendor directory

--- vendor/classiebit/eventmie/resources/views/vendor/voyager/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('voyager::master')
-
-@section('content')
-    <div class="page-content custom-dashboard">
-        
-        @include('voyager::alerts')
-        @include('voyager::dimmers')
-    </div>
-@stop
